2afd2bc896b54d848a18af38e08f0fbbbcf6b59a
IsaiasSPinto <isaiascxs10@gmail.com>
2023-01-28 09:19:47 -0300

    modulo 4 nullable

---
+++ Modulo 4/Nullable/Program.cs
@@ -0,0 +1,35 @@
+﻿double? x = null;
+double? y = 10;
+
+//se null retorna o valor padrao do tipo
+Console.WriteLine(x.GetValueOrDefault());
+Console.WriteLine(y.GetValueOrDefault());
+
+//retorna true or false
+System.Console.WriteLine(x.HasValue);
+System.Console.WriteLine(y.HasValue);
+
+if(x.HasValue) {
+    System.Console.WriteLine(x.Value);
+} else {
+    System.Console.WriteLine("X is null");
+}
+
+if(y.HasValue) {
+    System.Console.WriteLine(y.Value);
+} else {
+    System.Console.WriteLine("Y is null");
+}
+
+double a = x ?? 5;
+double b = y ?? 5;
+
+System.Console.WriteLine(a);
+System.Console.WriteLine(b);
+
+
+
+
+
+
+
