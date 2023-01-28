904d6f16cfa6ddafa6dfabe35dabd65e772c766c
IsaiasSPinto <isaiascxs10@gmail.com>
2023-01-28 09:53:54 -0300

    aulas vetores

---
+++ Modulo 4/Vetores/Product.cs
@@ -0,0 +1,9 @@
+namespace Vetores
+{
+    public class Product
+    {
+        public string Nome { get; set; }
+
+        public double Valor { get; set; }
+    }
+}
---
+++ Modulo 4/Vetores/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using Vetores;
+
+//Parte 1
+int n = int.Parse(Console.ReadLine());
+
+double[] vect = new double[n];
+
+double sum = 0;
+for(int i = 0; i < n; i++) {
+    vect[i] =  double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    sum += vect[0];
+}
+
+double media = sum / n;
+
+System.Console.WriteLine(media.ToString("F2",CultureInfo.InvariantCulture));
+
+//Parte 2
+System.Console.WriteLine("Quantos produtos serão cadastrados: ");
+int productQuantity = int.Parse(Console.ReadLine());
+
+Product[] products = new Product[productQuantity];
+
+double totalPrice = 0;
+
+for(int i = 0;i < productQuantity;i++) {
+    System.Console.WriteLine("Entre com o nome do produto: ");
+    string nome = Console.ReadLine();
+    System.Console.WriteLine("Entre com o preço do produto: ");
+    double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    products[i] = new Product {
+        Nome = nome,
+        Valor = valor
+    };
+
+    totalPrice += products[i].Valor;
+};
+
+double avaragePrice = totalPrice / productQuantity;
+
+System.Console.WriteLine("Average price = " + avaragePrice.ToString("F2", CultureInfo.InvariantCulture));
+
